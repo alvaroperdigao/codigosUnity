3b40c5c6c0ef83ac7b2aeac18e75607ff73a26ff
Álvaro Perdigão <35301435+alvaroperdigao@users.noreply.github.com>
2020-01-13 22:44:10 -0300

    Create controleMovimento.cs

---
+++ controleMovimento.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controleMovimento : MonoBehaviour {
+
+
+	static Animator anim;
+	static bool andando = false;
+	public int rotacao ;
+
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (andando == true) {
+			transform.Translate (Vector3.forward * Time.deltaTime);
+			transform.Rotate (Vector3.up * Time.deltaTime*rotacao);
+		}
+	}
+
+
+	static public void movimenta1() {
+		anim.SetBool("move1", true);
+		anim.SetBool("move2", false);
+		andando = true;
+
+	}
+
+	static public void movimenta2() {
+		anim.SetBool("move2", true);
+		anim.SetBool("move1", false);
+		andando = false;
+	}
+		
+}
